c748e66106969bf273f3d85254210e85a4d1ae6a
dhasenan <dhasenan@gmail.com>
2018-10-08 09:07:31 -0700

    Update allowed identifier characters to match C11.
    See http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf

---
+++ test/compilable/testuniident.d
@@ -0,0 +1,25 @@
+/************************************************/
+// Unicode identifiers matching C99
+// Only going with valid Unicode here
+
+struct 𐃌 
+{
+    int 𐂅 = 17;
+    string ꕌ = "VAI SYLLABLE HA";
+    dstring 𒁞
+        = "CUNEIFORM SIGN DAG KISIM5 TIMES GIR2"d;
+}
+
+bool ꯀꯒꯢ(𐃌 t)
+{
+    return true;
+}
+
+int main(string[] 𞤀𞤓𞤔𞤕)
+{
+    𐃌   ᑏᑲ;
+    if (ꯀꯒꯢ(ᑏᑲ)) return 0;
+    return 1;
+}
+
+/************************************************/
